9959e37fc55895fd6627ad6bf9175451f6142f96
keisuke1206 <keisukeeee12061206@gmail.com>
2023-01-14 21:46:45 +0900

    app/controllers/articles_controller.rbについて

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,23 @@
 class ArticlesController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def index
   end
 
   def show
     @id = params[:id]
   end
+
+  def update
+  end
+
+  def page1
+  end
+
+  def page2
+  end
+
+  def page3
+  end
+  
 end
