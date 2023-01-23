05a27110796a637257c83a375e0d7ff7974c4dfb
keisuke1206 <keisukeeee12061206@gmail.com>
2023-01-23 23:06:50 +0900

    config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get "/hello", to: "top#index"
   get "/articles", to: "articles#index"
   get "/articles/:id", to: "articles#show"
-  post "/articles/update/",to:"articles#update"
+  post "/articles/update",to:"articles#update"
   get "/loop_page/page1",to:"loop_page#page1"
   get "/loop_page/page2",to:"loop_page#page2"
-  get "/loop_page/page3",rato:"loop_page#page3"
+  get "/loop_page/page3",to:"loop_page#page3"
 end
