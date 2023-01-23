357f1e801ecd41adb12196f0154279a26665e314
keisuke1206 <keisukeeee12061206@gmail.com>
2023-01-23 23:04:09 +0900

    app/controllers/articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,12 +12,15 @@ class ArticlesController < ApplicationController
   end
 
   def page1
+    render template:"loop_page/page2"
   end
 
   def page2
+    render template:"loop_page/page3"
   end
 
   def page3
+    render template:"loop_page/page1"
   end
-  
+
 end
