73f1a2a088acd4b2909460cfdb055f6232271d6e
keisuke1206 <keisukeeee12061206@gmail.com>
2023-02-01 01:06:31 +0900

    app/controllers/loop_page_controller.rb

--- app/controllers/loop_page_controller.rb
+++ app/controllers/loop_page_controller.rb
@@ -5,15 +5,33 @@ def loop_page
 end
 
 def page1
-    render :text => "値 = #{params[:num]}"
+    Rails.logger.debug '---page1'
+    @hoge1 = params[:num]
+    render template:"loop_page/page1"
 end
 
 def page2
-    render :text => "値 = #{params[:num]}"
+    Rails.logger.debug '---page2'
+    @hoge2 = params[:num]
+    render template:"loop_page/page2"
 end
 
 def page3
+    Rails.logger.debug '---page3'
+    @hoge3 = params[:num]
     render template:"loop_page/page3"
+end
+
+def page4
+    Rails.logger.debug '---page4'
+end
+
+def page4_post
+    Rails.logger.debug '---page4'
+    Rails.logger.debug params
+    @hoge4 = "hoge1"
+    @hoge5 = params[:num]
+    render template:"loop_page/page5"
 end
 
 end
