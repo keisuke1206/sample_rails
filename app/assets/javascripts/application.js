cab72f97d99191c09cb0532b6ffd99dec0e94b33
keisuke1206 <keisukeeee12061206@gmail.com>
2023-02-18 19:57:57 +0900

    bootstrapについて

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,13 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require jquery #追加
-//= require jquery_ujs #追加
+//= require jquery
+//= require jquery_ujs
 
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
 
+import "bootstrap-sprockets";
 import "popper"
 import "bootstrap"
