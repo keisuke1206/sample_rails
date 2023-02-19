7e6f21d873a91f5a25b30b7a8cf65941fb316aa5
keisuke1206 <keisukeeee12061206@gmail.com>
2023-02-19 10:15:06 +0900

    app/assets/javascripts/application.jsの修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,3 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require jquery
-//= require jquery_ujs
-
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
-
-import "bootstrap-sprockets";
-import "popper"
-import "bootstrap"
